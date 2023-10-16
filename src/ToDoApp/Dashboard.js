8d068ad1ca4effc756c4f2ea0e42b580d59bf669
Avantika Sathaye <avantika.sathaye@innovyatech.com>
2023-10-16 16:17:11 +0530

    Updated css

--- src/ToDoApp/Dashboard.js
+++ src/ToDoApp/Dashboard.js
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 const Item = styled(Button)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
-  padding: theme.spacing(2),
+  padding: theme.spacing(6),
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
@@ -17,10 +17,10 @@ const Item = styled(Button)(({ theme }) => ({
 const Dashboard = () => {
   return (
     <Box sx={{ flexGrow: 1 , marginTop: "45px"}}>
-      <Grid container spacing={{ xs: 2, md: 5 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Grid container spacing={{ xs: 2, md: 5 }} columns={{ xs: 4, sm: 8, md: 12 }} style={{ justifyContent: "center"}}>
         
-          <Grid item xs={2} sm={4} md={4}>
-            <Item variant="contained" component={Link} to="/todos">To Do App</Item>
+          <Grid item xs={2} sm={4} md={4} style={{margin: "10px"}}>
+            <Item variant="contained" component={Link} to="/todos" style={{margin: "10px"}}>To Do App</Item>
             <Item variant="contained" component={Link} to="/cardsexample">Material UI Cards</Item>
           </Grid>
         
