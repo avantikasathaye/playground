93ab664be872ed35f2bba7de9d5dc6a38cd36527
Avantika Sathaye <avantika.sathaye@innovyatech.com>
2023-10-18 15:21:38 +0530

    Code cleanup

--- src/CountdownTimer/Timer.js
+++ src/CountdownTimer/Timer.js
@@ -1,54 +1,37 @@
 import { Box, Button } from '@mui/material';
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 const Timer = () => {
-
-   /*  const startTiming = () => {
-      var date = new Date(0);
-      date.setSeconds(10); 
-      var timeString = date.toISOString().substring(11, 19);
-      console.log(timeString)
-    } */
-
     var t = new Date();
-    var t_in_seconds = t.getSeconds();
+    var time_in_seconds = t.getSeconds();
 
     var newSeconds = t.setSeconds(t.getSeconds() + 10);
-    var diff = (Math.floor((newSeconds % (1000*60))/1000)) - t_in_seconds;
+    var diff = (Math.floor((newSeconds % (1000*60))/1000)) - time_in_seconds;
 
-    var t_in_minutes = t.getMinutes() - t.getMinutes();
-    var formatted_T_minutes = String(t_in_minutes).padStart(2,'0');
-    //console.log((Math.floor((newSeconds % (1000*60))/1000)) - t_in_seconds);
-
-    //const countDownDate = new Date(targetDate).getTime();
-    /* const [timerCount, setTimerCount] = useState(
-       Math.round(new Date().getTime() / 1000)
-    ) */
-
+    var time_in_minutes = t.getMinutes() - t.getMinutes();
+    var formatted_T_minutes = String(time_in_minutes).padStart(2,'0');
+    
     const [count, setCount] = useState(diff);
     const [pause, setPause] = useState(false);
+    const [message, setMessage] = useState("Countdown Timer");
 
     let intervalRef = useRef();
 
     const decrement = () => setCount((prev) => prev === 0 ? 0 : prev - 1);
 
-    /* useEffect(() => {
-        intervalRef.current = setInterval(decrement, 1000);
-        return () => clearInterval(intervalRef.current);
-    }, []); */
-
     const handlePause = () => {
-        /* if (!pause) {
-          clearInterval(intervalRef.current);
-        } else {
-          intervalRef.current = setInterval(decrement, 1000);
-        } */
-
+      if (!pause) {
+        setMessage("Countdown Paused ...")
         clearInterval(intervalRef.current);
-        setPause((prev) => !prev);
+      } else {
+        setMessage("Countdown Resumed ...")
+        intervalRef.current = setInterval(decrement, 1000);
+      }
+      setPause((prev) => !prev);
       };
 
     const handleStart = () => {
+        setMessage("Countdown Started ... ")
         intervalRef.current = setInterval(decrement, 1000);
     }
 
@@ -59,22 +42,19 @@ const Timer = () => {
 
   return (
     <>
-    <Box style={{display:"flex",
-                justifyContent:"center",
-                alignItems:"center"}}>
-
-      <div>{formatted_T_minutes} :{String(count).padStart(2, '0')}</div>
-    
+     <div className="centerAlign">
+      <h3>CountDown Timer</h3>
+    </div>
+    <Box className="centerAlign">
+      <div>{formatted_T_minutes} : {String(count).padStart(2, '0')}</div>
     </Box>
-    <Box  style={{display:"flex",
-                justifyContent:"center",
-                alignItems:"center",
-                minHeight:"1vh"}}>
-        
-      <Button style={{margin: "10px"}} variant="contained" onClick={handlePause}>Pause</Button>
-      <Button style={{margin: "10px"}} variant="contained" onClick={handleStart}>Start</Button>
+    <Box  className="centerAlign">
+      <Button style={{margin: "10px"}} variant="contained" onClick={handlePause} disabled={count === diff || count === 0}>{pause ? "Resume" : "Pause"}</Button>
+      <Button style={{margin: "10px"}} variant="contained" onClick={handleStart} disabled={count < 10}>Start</Button>
       <Button style={{margin: "10px"}} variant="contained" onClick={handleReset}>Reset</Button>
     </Box>
+
+    <p className='centerAlign'>{count === 0 ? "Countdown Completed!" : message}</p>
     </>
   )
 }
